3e0ef554becc18ff88d1e5f2fe978907509ea6a2
Biswo2002 <biswopaban.nayak@zorrroservices.com>
2026-04-24 19:54:57 +0530

    Add new assets including images, icons, and SVGs

---
+++ src/assets/icons/icon.ts
@@ -0,0 +1,9 @@
+export default {
+   WB_LOGO: require('./wb-logo.png'),
+   Toast: {
+      WARNING: require('./caution.png'),
+      SUCCESS: require('./success.png'),
+      ERROR: require('./close.png'),
+      INFO: require('./info.png'),
+    },
+};
---
+++ src/assets/svg/svg.ts
@@ -0,0 +1,87 @@
+import RIGHT_ARROW from './arrow_forward.svg';
+import AUDIO_SETTINGS from './edit_audio.svg';
+import CAMERA_ICON from './photo_camera.svg';
+import JOINING from './calendar_today.svg';
+import BENGAL_MAP from './bengal_map.svg';
+import DOC_SHEET from './doc-sheet.svg';
+import VIDEO_ICON from './videocam.svg';
+import GO_BACK from './arrow_left.svg';
+import LOCATION from './distance.svg';
+import DOWNLOAD from './download.svg';
+import DOCUMENT from './document.svg';
+import VIOLENCE from './violence.svg';
+import ACCIDENT from './accident.svg';
+import MATERNAL from './maternal.svg';
+import CHANNELS from './channel.svg';
+import DEVICE from './mobile_3.svg';
+import WB_LOGO from './wb-logo.svg';
+import END_TO_END from './lock.svg';
+import PROFILE from './profile.svg';
+import GALLERY from './gallery.svg';
+import WARNING from './warning.svg';
+import MEDICAL from './medical.svg';
+import HISTORY from './history.svg';
+import REVENUE from './domain.svg';
+import SEARCH from './search.svg';
+import CHATS from './message.svg';
+import CAMERA from './camera.svg';
+import OTHER from './other.svg';
+import CHILD from './child.svg';
+import EMAIL from './mail.svg';
+import LINK from './link.svg';
+import MORE from './more.svg';
+import AUDIO from './mic.svg';
+import PHONE from './call.svg';
+import SEND from './send.svg';
+import SOS from './sos.svg';
+export default {
+   BENGAL_MAP,
+   WB_LOGO,
+   TAB_LAYOUT: {
+      CHANNELS,
+      PROFILE,
+      CHATS,
+      SOS,
+   },
+
+   PROFILE_TAB: {
+      END_TO_END,
+      LOCATION,
+      JOINING,
+      REVENUE,
+      DEVICE,
+      EMAIL,
+      PHONE,
+   },
+
+   CHAT_SCREENS: {
+      AUDIO_SETTINGS,
+      DOC_SHEET,
+      DOCUMENT,
+      GALLERY,
+      CAMERA,
+      AUDIO,
+      SEND,
+   },
+
+   SCREENS: {
+      RIGHT_ARROW,
+      CAMERA_ICON,
+      VIDEO_ICON,
+      DOWNLOAD,
+      GO_BACK,
+      SEARCH,
+      MORE,
+      LINK,
+   },
+   SOS: {
+      WARNING,
+      HISTORY,
+      MEDICAL,
+      VIOLENCE,
+      ACCIDENT,
+      MATERNAL,
+      CHILD,
+      OTHER,
+   },
+};
